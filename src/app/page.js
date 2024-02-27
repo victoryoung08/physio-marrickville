4a8fe1068e5d2634fac95a40b111f02706ed4d12
victoryoung <vicmachine08@gmail.com>
2024-02-27 13:37:21 +1100

    initial build

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,120 @@
 import Image from "next/image";
 
 export default function Home() {
+  const services = [
+    {
+      heading: "Sports Injury and Rehabilitation",
+      imgSrc: "",
+    },
+    {
+      heading: "Massage Therapy",
+      imgSrc: "",
+    },
+    {
+      heading: "Exercise Rehabilitation",
+      imgSrc: "",
+    },
+    {
+      heading: "Pelvic Health",
+      imgSrc: "",
+    },
+    {
+      heading: "Dry Needling and Taping",
+      imgSrc: "",
+    },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <section className="flex gap-4 px-24 py-48">
+        <div className="w-full">
+          <h1>Expert Physiotherapists in Marrickville</h1>
+          <p>Get personalised pain and sports injury care</p>
         </div>
-      </div>
+        <div className="w-full flex flex-col gap-4 justify-center items-center">
+          <h2>Get in touch</h2>
+          <form
+            action=""
+            className="flex flex-col items-center justify-center gap-2"
+          >
+            <input type="text" placeholder="Enter Your Name" />
+            <input type="email" placeholder="Enter your Email" />
+            <input type="phone" placeholder="Enter your Phone" />
+            <input type="textarea" placeholder="How can we help?" />
+            <button className="is-full">Reach out</button>
+          </form>
+        </div>
+      </section>
+      {/* Feature Icon Row */}
+      <section className="bg-[#f7f9f9] px-24 py-16 flex">
+        <div className="flex flex-col gap-4 w-full items-center">
+          <div className="h-12 w-12 bg-gray-500">
+            <img src="" alt="" />
+          </div>
+          <h4>1000+ Patients</h4>
+        </div>
+        <div className="flex flex-col gap-4 w-full items-center">
+          <div className="h-12 w-12 bg-gray-500">
+            <img src="" alt="" />
+          </div>
+          <h4>Local & Proud</h4>
+        </div>
+        <div className="flex flex-col gap-4 w-full items-center">
+          <div className="h-12 w-12 bg-gray-500">
+            <img src="" alt="" />
+          </div>
+          <h4>Sports Injuries</h4>
+        </div>
+      </section>
+      {/* Feature Row */}
+      <section>
+        <div className={`w-full flex px-24 py-48 gap-[72px]`}>
+          <div className={`w-full order-first`}>
+            <h3 className="mb-4">Sports Inury and Rehabilitation</h3>
+            <p>sample text</p>
+            <div className="button-wrapper">
+              <button>Get Started</button>
+            </div>
+          </div>
+          <div className="w-full h-[300px] bg-gray-500 rounded-2xl overflow-hidden">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </section>
+      {/* About */}
+      <section className="px-24 py-24 flex flex-col gap-24">
+        <div className="flex">
+          <div className="w-full">
+            <h2 className="w-full">
+              Personalised, individualised and you focused Physiotherapy
+            </h2>
+            <div className="button-wrapper">
+              <button>Schedule an appointment</button>
+            </div>
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p className="w-full">
+            At Physio Marrickville, we focus on getting you back to where you
+            were and better. Our services include physical therapy, massage
+            therapy, dry needling and exercise rehabilitation. All of our
+            Physios are University Qualified with a wealth of experience so rest
+            assure you&apos;ll be in safe and dedicated hands helping you
+            recover. Our core focus is to help you recover from injuries, manage
+            and reduce plain while also improving your overall well-being.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </div>
+        <img className="w-full h-[450px] bg-gray-500 rounded-xl" />
+      </section>
+      {/* Services */}
+      <section className="grid grid-cols-3 gap-4 px-24 py-24">
+        {services.map((service) => (
+          <div className="service-card" key={service.heading}>
+            <img src="" alt="" className="w-full h-2/3 bg-gray-500" />
+            <div className="w-full h-1/3 flex items-center justify-center">
+              <h4>{service.heading}</h4>
+            </div>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }
